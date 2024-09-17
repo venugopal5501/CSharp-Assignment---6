744c1b816b67d7c09db901242e92dd28f6d61611
venugopal5501 <venugopal14082002@gmail.com>
2024-09-17 16:59:07 +0530

    circle

--- 3.shape.cs
+++ 3.shape.cs
@@ -1,23 +1,54 @@
 using System;
 
 
-class shape{
-    public virtual void getPerimeter(){
+class shape
+{
+    public virtual void getPerimeter()
+    {
+        Console.Write("Perimeter Calculation will be done here ");
 
     }
-    public virtual void getArea(){
+    public virtual void getArea()
+    {
+
     }
 }
 
-class Circle : shape{
+class Circle : shape
+{
+    double radius;
+    double area;
+
+    public Circle(double area, double radius)
+    {
+        this.area = area;
+        this.radius=radius;
+    }
+
     public override void getPerimeter()
     {
-        
+        double c = 2 * 3.14 * radius;
+        Console.WriteLine("The Perimeter of a circle is : " + c);
+
     }
 
     public override void getArea()
     {
-       
-    }
+        double d=3.14*area*area;
+        Console.WriteLine("The Area of a circle is : " +d);
 
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter Radius of a circle : ");
+        double area = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Radius of a circle : ");
+        double radius = Convert.ToInt32(Console.ReadLine());
+        Circle c = new Circle(area, radius);
+        c.getPerimeter();
+        c.getArea();
+    }
 }
