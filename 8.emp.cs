22a4ba4fe4063283e16961f2e2e3469ba00652fe
venugopal5501 <venugopal14082002@gmail.com>
2024-09-19 12:21:57 +0530

    calcsalary

--- 8.emp.cs
+++ 8.emp.cs
@@ -3,14 +3,19 @@ class Employee
 {
     public virtual void CalculateSalary()
     {
-
+        Console.Write("Employee salary calculations will be done here");
     }
 }
 class Manager : Employee
 {
     public override void CalculateSalary()
     {
-
+        Console.Write("Enter your base salary : ");
+        int salary = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter your year of experience : ");
+        int experience = Convert.ToInt32(Console.ReadLine());
+        int calc = salary + (experience * 5000);
+        Console.Write("Your salary is : " + calc);
     }
 
 }
@@ -19,13 +24,32 @@ class Programmer : Employee
 {
     public override void CalculateSalary()
     {
-
+        Console.Write("Enter your base salary : ");
+        int salary = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter your year of experience : ");
+        int experience = Convert.ToInt32(Console.ReadLine());
+        int calc = salary + (experience * 2500);
+        Console.Write("Your salary is : " + calc);
     }
 
 }
 
-class Program{
-    public static void Main(string []args){
-        
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your role : ");
+        string role = Console.ReadLine();
+        Manager MA = new Manager();
+        Programmer PM = new Programmer();
+        if (role.Equals("Manager"))
+        {
+            MA.CalculateSalary();
+        }
+        if (role.Equals("Programmer"))
+        {
+            PM.CalculateSalary();
+        }
+
     }
 }
