b5c1665c473363cf2e89175dff0921df0859d7f6
venugopal5501 <venugopal14082002@gmail.com>
2024-09-19 13:02:51 +0530

    flight

---
+++ 7.Airplance.cs
@@ -0,0 +1,36 @@
+using System;
+class Airplane
+{
+    public static void delay()
+    {
+        Console.Write("Enter Flight Number : ");
+        int flightnum = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Flight destination : ");
+        string destination = Console.ReadLine();
+        Console.Write("Enter Flight departure time : ");
+        int time = 7;
+        int departuretime = Convert.ToInt32(Console.ReadLine());
+        int delay = (time - departuretime);
+        Console.Write("Flight delay is : " + Math.Abs(delay) + " hr");
+        if (time == delay)
+        {
+            Console.WriteLine("Status : On- Time");
+        }
+        else if (time < delay)
+        {
+            Console.WriteLine("Status : Delayed");
+        }
+        else if (departuretime == 0)
+        {
+            Console.WriteLine("Status : Cancelled");
+        }
+
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Airplane.delay();
+    }
+}
